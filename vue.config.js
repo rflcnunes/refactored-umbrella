998c3a04a466bc549153c403ae491f944382f17e
rafael <rafaelcnunes@icloud.com>
2023-02-02 17:40:38 -0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+        @import "@/styles/scss/_variables.scss";
+        @import "@/styles/scss/_mixins.scss";
+        `,
+      },
+    },
+  },
+};
